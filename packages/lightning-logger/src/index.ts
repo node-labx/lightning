0c37785e6110b2711f70a3e42a32b60c35143190
kk <tzhwg0829@126.com>
2022-07-17 10:52:29 +0800

    chore: init workspace

---
+++ packages/lightning-logger/src/index.ts
@@ -0,0 +1,4 @@
+import Logger from './logger/Logger';
+import CoreLogger from './logger/CoreLogger';
+
+export { Logger, CoreLogger };
---
+++ packages/lightning-logger/src/logger/Logger.ts
@@ -0,0 +1,3 @@
+import CoreLogger from './CoreLogger';
+
+export default class Logger extends CoreLogger {}
